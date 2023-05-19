a0c517571abca86220aff2baf5426aee550a0b55
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-05-19 08:21:03 +0530

    Feature: Made mobile responsive

--- components/Receive.jsx
+++ components/Receive.jsx
@@ -24,20 +24,28 @@ function Receive(props) {
     };
     return (
         <div className=''>
-            {popUp
-                &&
+            {popUp && (
                 <div className='fixed top-0 left-0 right-0 bottom-0 text-center bg-gray-800 bg-opacity-50 flex flex-row items-center justify-center z-50'>
-                    <div className='bg-white rounded-lg overflow-hidden shadow-xl relative'>
-                        <button className='absolute top-0 right-0 p-4 mr-5 hover:scale-150 duration-100 transition ' onClick={togglePopUp}>X</button>
-                        <div className='p-20'>
-                            <QRCodeSVG className='text-center mx-auto' value={`ethereum:${props.userAddress}`} />
-                            <h1 className='mt-5 p-5 hover:cursor-pointer' ref={divRef} onClick={handleCopy}>{props.userAddress}</h1>
+                    <div className='bg-white rounded-lg overflow-hidden shadow-xl relative lg:mx-0 mx-5'>
+                        <button className='absolute top-0 right-0 p-4 mr-5 hover:scale-150 duration-100 transition' onClick={togglePopUp}>
+                            X
+                        </button>
+                        <div className='lg:p-20'>
+                            <div className='p-10 pb-5'>
+                                <QRCodeSVG className='text-center mx-auto' value={`ethereum:${props.userAddress}`} />
+                            </div>
+                            <h1 className='mt-5 p-5 pb-14 lg:pb-0  hover:cursor-pointer lg:text-lg sm:text-sm whitespace-normal max-h-32 overflow-y-auto' ref={divRef} onClick={handleCopy}>
+                                {props.userAddress}
+                            </h1>
                         </div>
                     </div>
                 </div>
-            }
-            <button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg ' onClick={togglePopUp}>Receive</button>
+            )}
+            <button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg' onClick={togglePopUp}>
+                Receive
+            </button>
         </div>
+
     )
 }
 
--- components/Send.jsx
+++ components/Send.jsx
@@ -67,8 +67,8 @@ function Send({ config, balance, init }) {
             {!popUp ? (
                 <button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg' onClick={togglePopUp}>Send</button>
             ) : (
-                <div className='fixed top-0 left-0 right-0 bottom-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50'>
-                    <div className='bg-white w-full sm:max-w-md rounded-lg overflow-hidden shadow-xl'>
+                <div className='fixed top-0 lg:m-0  left-0 right-0 bottom-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50'>
+                    <div className='bg-white w-full sm:max-w-md rounded-lg overflow-hidden shadow-xl lg:mx-0 mx-5'>
                         <div className='p-4'>
                             <div className='flex items-center justify-between'>
                                 <h2 className='text-2xl font-bold mb-4'>Send your Funds</h2>
