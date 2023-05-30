d7b0537c4140ed326ab9cee3672d1d294f800c6d
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-05-31 00:59:34 +0530

    Feature: Added context for Address

---
+++ components/contexts/AccountBalance.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const AccountBalance = createContext();
+
+export default AccountBalance;
---
+++ components/contexts/AccountInfo.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const AccountInfo = createContext();
+
+export default AccountInfo;
